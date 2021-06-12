3f5adaa22285c076fe91db6dd68e6a0e68cfc08b
MarkF791 <80972020+MarkF791@users.noreply.github.com>
2021-06-12 14:59:57 +0100

    Add project files.

---
+++ TestAppWebView/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestAppWebView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var webView = new WebView
+            {
+                Source = "http://127.0.0.1:8080/bluespec1/clientindex.php",
+                MinimumWidthRequest = 200,
+                MinimumHeightRequest = 200,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var stackLayout = new StackLayout();
+            this.Content = stackLayout;
+            stackLayout.Children.Add(webView);
+            stackLayout.HorizontalOptions = LayoutOptions.FillAndExpand;
+            stackLayout.VerticalOptions = LayoutOptions.FillAndExpand;
+        }
+    }
+}
